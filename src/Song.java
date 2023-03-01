165c99eb419ea4bc6564b077a6788a0bc1eaea33
amandaho <ahyh60@student.cis.edu.hk>
2023-03-01 08:06:33 +0800

    song calss aded

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,44 @@
+
+
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+
+    private ArrayList <String> oldPeople = new ArrayList();
+
+
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+    public int howMany(ArrayList <String> list){
+
+        int newPeople = 0;
+        for(String name : list){
+            if(!oldPeople.contains(name.toLowerCase())){
+                oldPeople.add(name.toLowerCase());
+                newPeople++;
+            }
+        }
+        return newPeople;
+    }
+
+
+
 }
+
+
+
+
